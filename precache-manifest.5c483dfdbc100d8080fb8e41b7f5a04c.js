e3705431229cca8d9642ca9952be681032ad29d2
alekseymakhankov <aleksey.makhankov@gmail.com>
2019-01-31 01:20:28 +0300

    Updates

---
+++ precache-manifest.5c483dfdbc100d8080fb8e41b7f5a04c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "800de3e9e4a97e4c4ae5",
+    "url": "/packages/static/js/runtime~main.800de3e9.js"
+  },
+  {
+    "revision": "0449e17d8297535aeb60",
+    "url": "/packages/static/js/main.0449e17d.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/packages/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "0449e17d8297535aeb60",
+    "url": "/packages/static/css/main.4ba7beb0.chunk.css"
+  },
+  {
+    "revision": "c08c3d044cb495665630225acbcef396",
+    "url": "/packages/index.html"
+  }
+];

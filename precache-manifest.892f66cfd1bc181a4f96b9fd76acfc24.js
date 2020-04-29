e7f22005ba78a10eb920ca0b7eca9439fa64f070
Aleksey Makhankov <aleksey.makhankov@innowise-group.com>
2020-04-29 23:25:35 +0300

    Updates

---
+++ precache-manifest.892f66cfd1bc181a4f96b9fd76acfc24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836bc31057c7a340c27ccc7404304439",
+    "url": "/index.html"
+  },
+  {
+    "revision": "492171e892a4cf144f36",
+    "url": "/static/css/main.194a9116.chunk.css"
+  },
+  {
+    "revision": "c3fe649387c1988d553b",
+    "url": "/static/js/2.cce2dfdb.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.cce2dfdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "492171e892a4cf144f36",
+    "url": "/static/js/main.deadf07e.chunk.js"
+  },
+  {
+    "revision": "86ae295571a0be6ee536",
+    "url": "/static/js/runtime-main.2492563f.js"
+  },
+  {
+    "revision": "2ff73863c6948e915f6d4767f82da604",
+    "url": "/static/media/github-icon.2ff73863.svg"
+  }
+]);
